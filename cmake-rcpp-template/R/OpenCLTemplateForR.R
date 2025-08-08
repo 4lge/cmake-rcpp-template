d30fc9402ad2d5ac8377daae54ca63e594ad1802
jmaerte <mail@maertej.com>
2025-08-08 14:30:03 +0200

    fixed wrongful commit

--- cmake-rcpp-template/R/OpenCLTemplateForR.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  kernelsPath <- system.file("kernels", package = pkgname)
-  print(kernelsPath)
-  .Call("SetKernelsPath", kernelsPath, PACKAGE = pkgname)
-}
--- cmake-rcpp-template/src/backend/include/backend/export.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Windows dll exports
-#ifdef _WIN32
-	#define EXPORT __declspec(dllexport)
-#else
-	#define EXPORT
-#endif
